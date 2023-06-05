9ab4fdfb07e873bedd1696badfc8099b0274bd85
Yury <matatov1989@gmail.com>
2023-06-05 10:08:35 +0300

    create project & read and use json objects

---
+++ app/src/main/java/com/example/simpleapicalldemo/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.simpleapicalldemo
+
+import android.app.Dialog
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        CallAPILoginAsyncTask().execute()
+    }
+
+    private inner class CallAPILoginAsyncTask() : AsyncTask<Any, Void, String>() {
+        private lateinit var customProgressDialog: Dialog
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            showProgressDialog()
+        }
+
+        override fun doInBackground(vararg params: Any?): String {
+            var result: String
+
+            var connection: HttpURLConnection? = null
+
+            try {
+                val url = URL("http://www.mocky.io/v2/5e3826143100006a00d37ffa")
+                connection = url.openConnection() as HttpURLConnection
+                connection.doInput = true
+                connection.doOutput = true
+                val httpResult: Int = connection.responseCode
+
+                if (httpResult == HttpURLConnection.HTTP_OK) {
+                    val inputStream = connection.inputStream
+                    val reader = BufferedReader(InputStreamReader(inputStream))
+                    val stringBuilder = StringBuilder()
+                    var line: String?
+
+                    try {
+                        while (reader.readLine().also { line = it } != null) {
+                            stringBuilder.append(line + "\n")
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    } finally {
+                        try {
+                            inputStream.close()
+                        } catch (e: IOException) {
+                            e.printStackTrace()
+                        }
+                    }
+                    result = stringBuilder.toString()
+                } else {
+                    result = connection.responseMessage
+                }
+            } catch (e: SocketTimeoutException) {
+                result = "Connection Timeout"
+            } catch (e: Exception) {
+                result = "Error: ${e.message}"
+            } finally {
+                connection?.disconnect()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            cancelProgressDialog()
+            Log.i("JSON_RESPONSE", result.toString())
+
+            val jsonObject = JSONObject(result)
+            val message = jsonObject.optString("message")
+            Log.i("JSON", message)
+            val userId = jsonObject.optString("user_id")
+            Log.i("JSON", userId)
+            val name = jsonObject.optString("name")
+            Log.i("JSON", name)
+
+            val profileDetailsObject = jsonObject.getJSONObject("profile_details")
+            val isProfileComplete = profileDetailsObject.getBoolean("is_profile_completed")
+            Log.i("JSON", "$isProfileComplete")
+
+            val dataListArray = jsonObject.optJSONArray("data_list")
+            Log.i("JSON", "${dataListArray.length()}")
+
+            for (item in 0 until dataListArray.length()) {
+                Log.i("JSON", "${dataListArray[item]}")
+
+                val dataItemObject: JSONObject = dataListArray[item] as JSONObject
+
+                val id = dataItemObject.optInt("id")
+                Log.i("JSON", "$id")
+
+                val value = dataItemObject.optString("value")
+                Log.i("JSON", "$value")
+            }
+        }
+
+        private fun showProgressDialog() {
+            customProgressDialog = Dialog(this@MainActivity)
+            customProgressDialog.setContentView(R.layout.dialog_custom_progress)
+            customProgressDialog.show()
+        }
+
+        private fun cancelProgressDialog() {
+            customProgressDialog.dismiss()
+        }
+
+    }
+
+}
